ad0d2ac08fc07eb57f69cc9c5eae361ad36db721
Karl Guzman <guzmankarl0410@gmail.com>
2021-01-01 20:57:34 -0800

    init

---
+++ src/AnotherFragments.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AnotherFragments() {
+    return (
+        <>
+            <h2>1st H2 element</h2>
+            <h2>2nd H2 element</h2>
+        </>
+    )
+}
+
+export default AnotherFragments
---
+++ src/Fragments.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+function Fragments() {
+    return (
+        <Fragment>
+            <h1>1st H1 element</h1>
+            <h1>2nd H1 element</h1>
+        </Fragment>
+    )
+}
+
+export default Fragments
